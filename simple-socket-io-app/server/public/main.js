db37bafc16819e4a515ec7abebab0f0270e45aec
Fagbenja Enoch <fagbenjaenoch73@gmail.com>
2023-04-07 11:10:51 +0100

    feat: adding chat functionality in js

--- simple-socket-io-app/server/public/main.js
+++ simple-socket-io-app/server/public/main.js
@@ -6,12 +6,34 @@ const chatBox = document.querySelector(".chat-box");
 
 // setupCounter(counter);
 
-const socket = new io();
+window.addEventListener("DOMContentLoaded", () => {
+  const socket = new io();
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  if (chatBox.value) {
-    socket.emit("chat message", chatBox.value);
-    chatBox.value = "";
-  }
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (chatBox.value) {
+      socket.emit("chat message", chatBox.value);
+      addChat(chat, chatBox.value);
+      chatBox.value = "";
+    }
+  });
 });
+
+/**
+ * @param baseChat Base Element to add chat message to
+ * @param newChat New chat to add to base element
+ */
+function addChat(baseChat, newChat) {
+  if (!baseChat || !newChat) {
+    return;
+  }
+
+  if (!(baseChat instanceof HTMLElement)) {
+    return;
+  }
+
+  const newChatEl = document.createElement("li");
+  newChatEl.classList.add("chat-bubble");
+  newChatEl.textContent = newChat;
+  baseChat.appendChild(newChatEl);
+}
